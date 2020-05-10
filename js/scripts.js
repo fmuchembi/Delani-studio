09bb8a9894d552e36c59a0f95b4d59378763daeb
faith <fmuchembi@gmail.com>
2020-05-10 17:03:03 +0300

    added hide and show jquery on my services section

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+ $(document).ready(function () {
+
+    $("#design").click(function(){
+        $("#paragraph1").show();
+    })
+
+    $("#service").click(function () {
+         $("#paragraph2").show();
+    })
+
+    $("#product").click(function () {
+         $("#paragraph3").show();
+    })
+
+
+
+});
+
+
